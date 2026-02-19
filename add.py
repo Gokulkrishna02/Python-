e12e25e66c1ba31d6a3f5a59552c5da26969bb42
Gokulkrishna02 <gokulkrishnakumaravel@gmail.com>
2026-02-19 08:50:55 +0530

    gnc

--- add.py
+++ add.py
@@ -1,2 +1,9 @@
-for i in range(1,5):
-    print("*"*i)
+# Take input from user
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
